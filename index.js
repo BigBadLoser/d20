e0f9cfb4297be620fcd9a0d29eb83da5af8350ae
Trey Gaulin <loserizer@gmail.com>
2018-10-02 23:53:33 -0400

    can roll any amount of dice!

--- index.js
+++ index.js
@@ -20,18 +20,32 @@ client.on('ready', () => {
 client.on('message', message => {
   // If the message is "ping"
   if (message.content.startsWith(".roll")) {
-    var messageSplit = message.content.split(" d");
-    Math.floor(Math.random() * 10) + 1;
+    var messageSplit = message.content.split(' ').join(',').split('d').join(',').split(',')
     if (isNaN(messageSplit[1])){
-          message.channel.send(messageSplit[1] + " is not a number!");
+          //message.channel.send(messageSplit[1] + " is not a number!");
     }
-    else {
+    else if (messageSplit[1].valueOf() == "".valueOf()){
       var rollOutcome = Math.floor(Math.random() * messageSplit[1]) + 1;
-      message.channel.send(":game_die: " + message.author + " rolled a d" + messageSplit[1] + " and got " + rollOutcome);
+      message.channel.send(":game_die: " + message.author + " rolled a d" + messageSplit[1] + " and got a" + rollOutcome);
       message.delete();
+
+    }
+    else{
+      var i, output = "[", rollOutcome, total = 0;
+      for (i = 0; i < messageSplit[1]; i++) {
+          rollOutcome = Math.floor(Math.random() * messageSplit[1]) + 1;
+          total += rollOutcome;
+          if (i != messageSplit[1] - 1){
+          output += (rollOutcome + " , ");}
+          else {
+            output += (rollOutcome + "]");
+          }
+      }
+
+      message.channel.send(":game_die: " + message.author + " rolled " + messageSplit[1] + "d" + messageSplit[2] + "s and got: " + output + " for a total of "+ total);
     }
   }
 });
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
-client.login('NDM3NjI2MDMyOTM0MjIzODc0.DpXDAg.xItO_EXQH6RRqXrwk1riM8hgtPY');
+client.login('token');
